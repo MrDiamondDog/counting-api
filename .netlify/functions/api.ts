5bcdd1cfb74f1270950dce80c4fbaccec5a9d136
MrDiamondDog <84212701+MrDiamondDog@users.noreply.github.com>
2023-08-04 18:47:35 -0600

    ughhh

--- .netlify/functions/api.ts
+++ .netlify/functions/api.ts
@@ -8,11 +8,11 @@ let num = 0;
 const router = Router();
 router.get("/", (req, res) => {
     num++;
-    res.sendStatus(200).send(`${num}`);
+    res.sendStatus(200).send({number: num, previous: num - 1});
 });
 
 router.get("/number", (req, res) => {
-    res.sendStatus(200).send(`${num}`);
+    res.sendStatus(200).send({number: num});
 });
 
 api.use("/api/", router);
